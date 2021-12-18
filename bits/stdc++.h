c17d91aa5b38540e74bdb579c43ce13aa4406797
Nathan Wong <NathanWongNZK@gmail.com>
2021-12-18 18:34:33 +0800

    Update 2021-12-18

--- bits/stdc++.h
+++ bits/stdc++.h
@@ -1,8 +1,37 @@
-#include <iostream>
-#include <iomanip>
+#include <cmath>
+#include <ctime>
+
+// C++
 #include <algorithm>
-#include <vector>
+#include <complex>
+#include <deque>
+#include <iomanip>
+#include <iostream>
+#include <list>
 #include <map>
 #include <queue>
-#include <string>
+#include <set>
 #include <stack>
+#include <string>
+#include <vector>
+#include <array>
+#include <random>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+
+/*
+#include <filesystem>
+#include <optional>
+#include <string_view>
+
+#include <future>
+#include <shared_mutex>
+
+#include <bitset>
+#include <ratio>
+#include <regex>
+#include <chrono>
+#include <thread>
+#include <mutex>
+*/
